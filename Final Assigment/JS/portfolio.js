2688694e72080b838307a248b41dd825770f6eae
Wrymouth38 <108835100+Wrymouth38@users.noreply.github.com>
2026-09-01 11:40:24 +0100

    writing my Java Script for slides and dots

--- Final Assigment/JS/portfolio.js
+++ Final Assigment/JS/portfolio.js
@@ -1,1 +1,25 @@
-alert("JavaScript is connected!");
+var slideIndex = 1;
+shoSlides(slideIndex);
+
+function showSlides(n) {
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+
+    for (var i = 0; i < slide.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+
+    for (var i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+}

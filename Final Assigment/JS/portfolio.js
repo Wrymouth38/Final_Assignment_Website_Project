2c84362b16a3af92dac2e584f230369b588a36c3
Wrymouth38 <108835100+Wrymouth38@users.noreply.github.com>
2026-09-01 12:57:15 +0100

    Update portfolio.js

--- Final Assigment/JS/portfolio.js
+++ Final Assigment/JS/portfolio.js
@@ -38,4 +38,7 @@ function openForm() {
     document.getElementById("myForm").style.display = "block";
 }
 
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
 
